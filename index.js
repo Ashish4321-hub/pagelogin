a6685ff186dd63812f26e90612d897dac37b79d0
Ashish4321-hub <anmol2001anu@gmail.com>
2022-01-30 17:59:19 +0530

    commit changes

--- index.js
+++ index.js
@@ -13,4 +13,26 @@ var Person = mangoose.model("Person",personSchema);
 app.get('/person', function(req, res){
   res.render('person');
 });
+app.post('/person', function(req, res){
+  var personInfo = req.body; //Get the parsed information
+  
+  if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+     res.render('show_message', {
+        message: "Sorry, you provided worng info", type: "error"});
+  } else {
+     var newPerson = new Person({
+        name: personInfo.name,
+        age: personInfo.age,
+        nationality: personInfo.nationality
+     });
+   
+     newPerson.save(function(err, Person){
+        if(err)
+           res.render('show_message', {message: "Database error", type: "error"});
+        else
+           res.render('show_message', {
+              message: "New person added", type: "success", person: personInfo});
+     });
+  }
+});
 app.listen(3000);
